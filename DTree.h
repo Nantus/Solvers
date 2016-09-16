7381291b3c743d42abd695db6f39a67d0d852b84
nantus <nantuspost@gmail.com>
2016-09-16 14:55:29 +0400

    Some new features

--- DTree.h
+++ DTree.h
@@ -38,14 +38,53 @@ class DTree{
             add(keyDataType,data,&(*node)->Right);
     }
 
+    myType toMyType(myType value){}
+
+    myType canculate(myType keyDataType, myType num1, myType num2){
+        switch(keyDataType){
+            case compilerDataTypes::operator_Plus:
+                return num1 + num2;
+
+            case compilerDataTypes::operator_Minus:
+                return num1 - num2;
+
+            case compilerDataTypes::operator_Multiple:
+                return num1 * num2;
+
+            case compilerDataTypes::operator_Division:
+                if(num2 != 0)
+                    return num1 / num2;
+        }
+    }
+
+    void compile(Node** node, std::vector<std::tuple<char,myType> >* variables){
+        if(    (*node)->keyDataType == compilerDataTypes::operator_
+           && ((*node)->Left->keyDataType == compilerDataTypes::constant
+           ||  (*node)->Left->keyDataType == compilerDataTypes::variable)
+           && ((*node)->Right->keyDataType == compilerDataTypes::constant
+           ||  (*node)->Right->keyDataType == compilerDataTypes::variable)) {
+            (*node)->keyDataType = compilerDataTypes ::constant;
+            (*node)->data = conculate((*node)->data,(*node)->Left->data,(*node)->Right->data);
+        }
+        else
+            compile(&(*node)->Left, variables);
+    }
+
     Node* Root;
+
+    myType mValue;
 public:
     DTree(){
-        Root = nullptr;
+        Root   = nullptr;
+        mValue = 0;
     }
 
     void Add(int keyDataType,myType data){
         add(keyDataType,data,&Root);
+    }
+
+    void Compile(std::vector<std::tuple<char,myType> >* variables){
+        compile(&Root,variables);
     }
 };
 
